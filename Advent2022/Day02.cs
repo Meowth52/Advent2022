9a905d9a27df64cf842c4cad074d26dcc9aabba0
Meowth <NoThisIsAPublicRepo@No.No>
2022-12-02 11:29:48 +0100

    Day 2

--- Advent2022/Day02.cs
+++ Advent2022/Day02.cs
@@ -9,11 +9,28 @@ namespace Advent2022
 {
     public class Day02 : Day
     {
-        List<int> Instructions;
+        List<string[]> Instructions;
+        Dictionary<string, int> MaterialValue;
+        Dictionary<string, string> MaterialVinnablility;
         public Day02(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Instructions = this.ParseListOfInteger(Input);
+            Input = Input.Replace("A", "X");
+            Input = Input.Replace("B", "Y");
+            Input = Input.Replace("C", "Z");
+            Instructions = this.ParseListOfStringArrays2(Input);
+            MaterialValue = new Dictionary<string, int>()
+            {
+                {"X", 1 } ,
+                {"Y", 2},
+                {"Z", 3}
+            };
+            MaterialVinnablility = new Dictionary<string, string>()
+            {
+                {"X", "Z"},
+                {"Y", "X"},
+                {"Z", "Y"}
+            };
         }
         public override Tuple<string, string> GetResult()
         {
@@ -22,14 +39,45 @@ namespace Advent2022
         public string GetPartOne()
         {
             int ReturnValue = 0;
-
+            foreach (string[] instruction in Instructions)
+            {
+                ReturnValue += MaterialValue[instruction[1]];
+                ReturnValue += IsWin(instruction);
+            }
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
         {
             int ReturnValue = 0;
 
+            foreach (string[] instruction in Instructions)
+            {
+                switch (instruction[1])
+                {
+                    case "X": //loose
+                        ReturnValue += MaterialValue[MaterialVinnablility[instruction[0]]];
+                        break;
+                    case "Y": //draw
+                        ReturnValue += MaterialValue[instruction[0]] + 3;
+                        break;
+                    case "Z": //win
+                        string key = MaterialVinnablility.FirstOrDefault(x => x.Value == instruction[0]).Key;
+                        ReturnValue += MaterialValue[key] + 6;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
             return ReturnValue.ToString();
+        }
+        int IsWin(string[] instruction)
+        {
+            if (instruction[0] == MaterialVinnablility[instruction[1]])
+                return 6;
+            else if (instruction[1] == MaterialVinnablility[instruction[0]])
+                return 0;
+            return 3;
         }
     }
 }
