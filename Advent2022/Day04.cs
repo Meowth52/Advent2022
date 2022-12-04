e2b08e90739ca248e42ee86206a2a0860d0a6539
Meowth52 <meowth@meowth.se>
2022-12-04 17:03:02 +0100

    day 4

--- Advent2022/Day04.cs
+++ Advent2022/Day04.cs
@@ -9,11 +9,12 @@ namespace Advent2022
 {
     public class Day04 : Day
     {
-        List<int> Instructions;
+        List<List<int>> Instructions;
         public Day04(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Instructions = this.ParseListOfInteger(Input);
+            Input = Input.Replace("-", "#");
+            Instructions = this.ParseListOfIntegerLists(Input);
         }
         public override Tuple<string, string> GetResult()
         {
@@ -22,13 +23,25 @@ namespace Advent2022
         public string GetPartOne()
         {
             int ReturnValue = 0;
-
+            foreach (List<int> pair in Instructions)
+            {
+                if ((pair[0] >= pair[2] && pair[1] <= pair[3]) || (pair[2] >= pair[0] && pair[3] <= pair[1]))
+                {
+                    ReturnValue++;
+                }
+            }
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
         {
             int ReturnValue = 0;
-
+            foreach (List<int> pair in Instructions)
+            {
+                if ((pair[0] <= pair[3] && pair[1] >= pair[2]) || (pair[2] >= pair[1] && pair[3] <= pair[0]))
+                {
+                    ReturnValue++;
+                }
+            }
             return ReturnValue.ToString();
         }
     }
