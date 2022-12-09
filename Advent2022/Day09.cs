388ebda4f2c77463a3d31e5e577d998be54b237d
Meowth <NoThisIsAPublicRepo@No.No>
2022-12-09 14:29:45 +0100

    Day 9 part 1 ok part 2 ring en vän

--- Advent2022/Day09.cs
+++ Advent2022/Day09.cs
@@ -9,11 +9,11 @@ namespace Advent2022
 {
     public class Day09 : Day
     {
-        List<int> Instructions;
+        List<string[]> Instructions;
         public Day09(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Instructions = this.ParseListOfInteger(Input);
+            Instructions = this.ParseListOfStringArrays2(Input);
         }
         public override Tuple<string, string> GetResult()
         {
@@ -22,13 +22,87 @@ namespace Advent2022
         public string GetPartOne()
         {
             int ReturnValue = 0;
-
+            HashSet<Coordinate> VisistedPositions = new HashSet<Coordinate>();
+            Coordinate Head = new Coordinate(0, 0);
+            Coordinate Tail = new Coordinate(0, 0);
+            VisistedPositions.Add(new Coordinate(Tail));
+            foreach (string[] instruction in Instructions)
+            {
+                char Direction = instruction[0][0];
+                int Steps = Int32.Parse(instruction[1]);
+                for (int i = 0; i < Steps; i++)
+                {
+                    Head.MoveNSteps(Direction);
+                    if (Head.ManhattanDistance(Tail) == 3)
+                    {
+                        Tail.x += Math.Sign(Head.x - Tail.x);
+                        Tail.y += Math.Sign(Head.y - Tail.y);
+                    }
+                    else if (Math.Abs(Head.x - Tail.x) == 2)
+                    {
+                        Tail.x += Math.Sign(Head.x - Tail.x);
+                    }
+                    else if (Math.Abs(Head.y - Tail.y) == 2)
+                    {
+                        Tail.y += Math.Sign(Head.y - Tail.y);
+                    }
+                    if (!VisistedPositions.Contains(Tail))
+                    {
+                        VisistedPositions.Add(new Coordinate(Tail));
+                    }
+                }
+            }
+            ReturnValue = VisistedPositions.Count;
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
         {
             int ReturnValue = 0;
+            HashSet<Coordinate> VisistedPositions = new HashSet<Coordinate>();
+            Coordinate Head = new Coordinate(0, 0);
+            List<Coordinate> Tail = new List<Coordinate>();
+            for (int i = 0; i < 9; i++)
+            {
+                Tail.Add(new Coordinate(0, 0));
+            }
+            VisistedPositions.Add(new Coordinate(Tail.Last()));
+            foreach (string[] instruction in Instructions)
+            {
+                char Direction = instruction[0][0];
+                int Steps = Int32.Parse(instruction[1]);
+                for (int i = 0; i < Steps; i++)
+                {
+                    Head.MoveNSteps(Direction);
+                    for (int i2 = 0; i2 < Tail.Count; i2++)
+                    {
+                        Coordinate Knot = Tail[i2];
+                        Coordinate Last;
+                        if (i2 == 0)
+                            Last = Head;
+                        else
+                            Last = Tail[i2 - 1];
 
+                        if (Last.ManhattanDistance(Knot) == 3)
+                        {
+                            Knot.x += Math.Sign(Last.x - Knot.x);
+                            Knot.y += Math.Sign(Last.y - Knot.y);
+                        }
+                        else if (Math.Abs(Last.x - Knot.x) == 2)
+                        {
+                            Knot.x += Math.Sign(Last.x - Knot.x);
+                        }
+                        else if (Math.Abs(Last.y - Knot.y) == 2)
+                        {
+                            Knot.y += Math.Sign(Last.y - Knot.y);
+                        }
+                        if (i2 == Tail.Count - 1 && !VisistedPositions.Contains(Knot))
+                        {
+                            VisistedPositions.Add(new Coordinate(Knot));
+                        }
+                    }
+                }
+            }
+            ReturnValue = VisistedPositions.Count;
             return ReturnValue.ToString();
         }
     }
---
+++ Advent2022Tests/Day09Tests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Advent2022;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2022.Tests
+{
+    [TestClass()]
+    public class Day09Tests
+    {
+        [TestMethod()]
+        public void GetPartOneTest()
+        {
+            Day09 day = new Day09("R 4\r\nU 4\r\nL 3\r\nD 1\r\nR 4\r\nD 1\r\nL 5\r\nR 2");
+            Assert.AreEqual("13", day.GetPartOne());
+        }
+
+        [TestMethod()]
+        public void GetPartTwoTest()
+        {
+            Day09 day = new Day09("R 5\r\nU 8\r\nL 8\r\nD 3\r\nR 17\r\nD 10\r\nL 25\r\nU 20");
+            Assert.AreEqual("36", day.GetPartTwo());
+        }
+    }
+}
