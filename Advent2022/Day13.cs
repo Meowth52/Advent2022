4b5f99aff0fa1b34dc46fd75e7774deb567510b5
Meowth52 <meowth@meowth.se>
2022-12-13 23:14:59 +0100

    Day 13 part 2

--- Advent2022/Day13.cs
+++ Advent2022/Day13.cs
@@ -12,11 +12,12 @@ namespace Advent2022
     public class Day13 : Day
     {
         List<string[]> Instructions;
+        string[] Instructions2;
         public Day13(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
             Instructions = this.ParseListOfStringArraysLineLine(Input);
-
+            Instructions2 = this.ParseStringArray(Input);
         }
         public override Tuple<string, string> GetResult()
         {
@@ -28,54 +29,27 @@ namespace Advent2022
             int i = 0;
             foreach (string[] strings in Instructions)
             {
-                Queue<int> One = Parse(strings[0]);
-                Queue<int> Two = Parse(strings[1]);
                 i++;
-                bool GetOut = false;
-                while (!GetOut)
-                {
-                    int one = One.Dequeue();
-                    int two = Two.Dequeue();
-                    if (one == two)
-                        ;
-                    else if (one == -1)
-                    {
-                        ReturnValue += i;
-                        break;
-                    }
-                    else if (one == -2 && two >= 0)
-                    {
-                        Two = new Queue<int>(Two.Prepend(-1));
-                        Two = new Queue<int>(Two.Prepend(two));
-
-                    }
-                    else if (two == -1)
-                    {
-                        break;
-                    }
-                    else if (two == -2 && one >= 0)
-                    {
-                        One = new Queue<int>(One.Prepend(-1));
-                        One = new Queue<int>(One.Prepend(one));
-                    }
-
-                    else if (one < two)
-                    {
-                        ReturnValue += i;
-                        break;
-                    }
-                    else if (one > two)
-                    {
-                        break;
-                    }
-                }
+                if (CompareStrings(strings[0], strings[1]))
+                    ReturnValue += i;
             }
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
         {
             int ReturnValue = 0;
-
+            int DividerIndexOne = 0;
+            int DividerIndexTwo = 0;
+            string DividerOne = "[[2]]";
+            string DividerTwo = "[[6]]";
+            foreach (string s in Instructions2)
+            {
+                if (CompareStrings(s, DividerOne))
+                    DividerIndexOne++;
+                if (CompareStrings(s, DividerTwo))
+                    DividerIndexTwo++;
+            }
+            ReturnValue = (DividerIndexOne + 1) * (DividerIndexTwo + 2);
             return ReturnValue.ToString();
         }
         public Queue<int> Parse(string input)
@@ -112,6 +86,51 @@ namespace Advent2022
                 }
             }
             return result;
+        }
+        public bool CompareStrings(string _one, string _two)
+        {
+            bool Win = false;
+            Queue<int> One = Parse(_one);
+            Queue<int> Two = Parse(_two);
+            bool GetOut = false;
+            while (!GetOut)
+            {
+                int one = One.Dequeue();
+                int two = Two.Dequeue();
+                if (one == two)
+                    ;
+                else if (one == -1)
+                {
+                    Win = true;
+                    break;
+                }
+                else if (one == -2 && two >= 0)
+                {
+                    Two = new Queue<int>(Two.Prepend(-1));
+                    Two = new Queue<int>(Two.Prepend(two));
+
+                }
+                else if (two == -1)
+                {
+                    break;
+                }
+                else if (two == -2 && one >= 0)
+                {
+                    One = new Queue<int>(One.Prepend(-1));
+                    One = new Queue<int>(One.Prepend(one));
+                }
+
+                else if (one < two)
+                {
+                    Win = true;
+                    break;
+                }
+                else if (one > two)
+                {
+                    break;
+                }
+            }
+            return Win;
         }
     }
 }
--- Advent2022Tests/Day13Tests.cs
+++ Advent2022Tests/Day13Tests.cs
@@ -22,7 +22,7 @@ namespace Advent2022.Tests
         public void GetPartTwoTest()
         {
             Day13 day = new Day13("[1,1,3,1,1]\r\n[1,1,5,1,1]\r\n\r\n[[1],[2,3,4]]\r\n[[1],4]\r\n\r\n[9]\r\n[[8,7,6]]\r\n\r\n[[4,4],4,4]\r\n[[4,4],4,4,4]\r\n\r\n[7,7,7,7]\r\n[7,7,7]\r\n\r\n[]\r\n[3]\r\n\r\n[[[]]]\r\n[[]]\r\n\r\n[1,[2,[3,[4,[5,6,7]]]],8,9]\r\n[1,[2,[3,[4,[5,6,0]]]],8,9]");
-            Assert.AreEqual("0", day.GetPartTwo());
+            Assert.AreEqual("140", day.GetPartTwo());
         }
     }
 }
