988bb035522d4ef7295e990147c79015b9b3d761
Meowth <NoThisIsAPublicRepo@No.No>
2022-12-13 16:39:33 +0100

    Day 13 example data is crap

--- Advent2022/Day13.cs
+++ Advent2022/Day13.cs
@@ -4,16 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.Text.Json.Serialization;
 
 namespace Advent2022
 {
     public class Day13 : Day
     {
-        List<int> Instructions;
+        List<string[]> Instructions;
         public Day13(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Instructions = this.ParseListOfInteger(Input);
+            Instructions = this.ParseListOfStringArraysLineLine(Input);
+
         }
         public override Tuple<string, string> GetResult()
         {
@@ -22,7 +24,27 @@ namespace Advent2022
         public string GetPartOne()
         {
             int ReturnValue = 0;
-
+            foreach (string[] strings in Instructions)
+            {
+                Queue<int> One = Parse(strings[0]);
+                Queue<int> Two = Parse(strings[1]);
+                int i = 0;
+                while (One.Count() > 0 && Two.Count > 0)
+                {
+                    int one = One.Dequeue();
+                    int two = Two.Dequeue();
+                    if (one < two)
+                    {
+                        ReturnValue += i + 1;
+                        break;
+                    }
+                    else if (one > two)
+                    {
+                        break;
+                    }
+                    i++;
+                }
+            }
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
@@ -30,6 +52,41 @@ namespace Advent2022
             int ReturnValue = 0;
 
             return ReturnValue.ToString();
+        }
+        public Queue<int> Parse(string input)
+        {
+            Queue<int> result = new Queue<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                switch (input[i])
+                {
+                    case '[':
+                        ;
+                        break;
+                    case ',':
+                        ;
+                        break;
+                    case ']':
+                        result.Enqueue(-1);
+                        break;
+                    default:
+                        string number = input[i].ToString();
+                        while (i < input.Length - 1)
+                        {
+                            if (Char.IsDigit(input[i + 1]))
+                            {
+                                i++;
+                                number += input[i].ToString();
+                            }
+                            else
+                                break;
+                        }
+                        result.Enqueue(Int32.Parse(number));
+                        break;
+                }
+            }
+            return result;
         }
     }
 }
---
+++ Advent2022Tests/Day13Tests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Advent2022;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2022.Tests
+{
+    [TestClass()]
+    public class Day13Tests
+    {
+        [TestMethod()]
+        public void GetPartOneTest()
+        {
+            Day13 day = new Day13("[1,1,3,1,1]\r\n[1,1,5,1,1]\r\n\r\n[[1],[2,3,4]]\r\n[[1],4]\r\n\r\n[9]\r\n[[8,7,6]]\r\n\r\n[[4,4],4,4]\r\n[[4,4],4,4,4]\r\n\r\n[7,7,7,7]\r\n[7,7,7]\r\n\r\n[]\r\n[3]\r\n\r\n[[[]]]\r\n[[]]\r\n\r\n[1,[2,[3,[4,[5,6,7]]]],8,9]\r\n[1,[2,[3,[4,[5,6,0]]]],8,9]");
+            Assert.AreEqual("13", day.GetPartOne());
+        }
+
+        [TestMethod()]
+        public void GetPartTwoTest()
+        {
+            Day13 day = new Day13("[1,1,3,1,1]\r\n[1,1,5,1,1]\r\n\r\n[[1],[2,3,4]]\r\n[[1],4]\r\n\r\n[9]\r\n[[8,7,6]]\r\n\r\n[[4,4],4,4]\r\n[[4,4],4,4,4]\r\n\r\n[7,7,7,7]\r\n[7,7,7]\r\n\r\n[]\r\n[3]\r\n\r\n[[[]]]\r\n[[]]\r\n\r\n[1,[2,[3,[4,[5,6,7]]]],8,9]\r\n[1,[2,[3,[4,[5,6,0]]]],8,9]");
+            Assert.AreEqual("0", day.GetPartTwo());
+        }
+    }
+}
