a4e455ac89969fe028db3ce52d1860e8d7796801
Meowth52 <meowth@meowth.se>
2022-12-13 22:49:34 +0100

    day 13 part 1

--- Advent2022/Day13.cs
+++ Advent2022/Day13.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.Text.Json.Serialization;
+using System.Xml;
 
 namespace Advent2022
 {
@@ -24,25 +25,49 @@ namespace Advent2022
         public string GetPartOne()
         {
             int ReturnValue = 0;
+            int i = 0;
             foreach (string[] strings in Instructions)
             {
                 Queue<int> One = Parse(strings[0]);
                 Queue<int> Two = Parse(strings[1]);
-                int i = 0;
-                while (One.Count() > 0 && Two.Count > 0)
+                i++;
+                bool GetOut = false;
+                while (!GetOut)
                 {
                     int one = One.Dequeue();
                     int two = Two.Dequeue();
-                    if (one < two)
+                    if (one == two)
+                        ;
+                    else if (one == -1)
                     {
-                        ReturnValue += i + 1;
+                        ReturnValue += i;
+                        break;
+                    }
+                    else if (one == -2 && two >= 0)
+                    {
+                        Two = new Queue<int>(Two.Prepend(-1));
+                        Two = new Queue<int>(Two.Prepend(two));
+
+                    }
+                    else if (two == -1)
+                    {
+                        break;
+                    }
+                    else if (two == -2 && one >= 0)
+                    {
+                        One = new Queue<int>(One.Prepend(-1));
+                        One = new Queue<int>(One.Prepend(one));
+                    }
+
+                    else if (one < two)
+                    {
+                        ReturnValue += i;
                         break;
                     }
                     else if (one > two)
                     {
                         break;
                     }
-                    i++;
                 }
             }
             return ReturnValue.ToString();
@@ -62,7 +87,7 @@ namespace Advent2022
                 switch (input[i])
                 {
                     case '[':
-                        ;
+                        result.Enqueue(-2);
                         break;
                     case ',':
                         ;
