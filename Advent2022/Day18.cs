948d09e7da19a1b48991ff39efb1513fbe3b59aa
Meowth52 <meowth@meowth.se>
2022-12-18 21:56:28 +0100

    Day 18

--- Advent2022/Day18.cs
+++ Advent2022/Day18.cs
@@ -9,11 +9,16 @@ namespace Advent2022
 {
     public class Day18 : Day
     {
-        List<int> Instructions;
+        List<List<int>> Instructions;
+        HashSet<Cooschmoordinate> Schmoshmoordinates;
         public Day18(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Instructions = this.ParseListOfInteger(Input);
+            Instructions = this.ParseListOfIntegerLists(Input); Schmoshmoordinates = new HashSet<Cooschmoordinate>();
+            foreach (List<int> i in Instructions)
+            {
+                Schmoshmoordinates.Add(new Cooschmoordinate(i[0], i[1], i[2]));
+            }
         }
         public override Tuple<string, string> GetResult()
         {
@@ -22,13 +27,51 @@ namespace Advent2022
         public string GetPartOne()
         {
             int ReturnValue = 0;
-
+            foreach (Cooschmoordinate c in Schmoshmoordinates)
+            {
+                List<Cooschmoordinate> Schneigbours = c.GetNeihbours();
+                foreach (Cooschmoordinate sch in Schneigbours)
+                    if (!Schmoshmoordinates.Contains(sch))
+                        ReturnValue++;
+            }
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
         {
             int ReturnValue = 0;
-
+            int MaxX = 0;
+            int MaxY = 0;
+            int MaxZ = 0;
+            foreach (Cooschmoordinate c in Schmoshmoordinates)
+            {
+                if (c.x > MaxX)
+                    MaxX = c.x;
+                if (c.y > MaxY)
+                    MaxY = c.y;
+                if (c.z > MaxZ)
+                    MaxZ = c.z;
+            }
+            HashSet<Cooschmoordinate> Kollat = new HashSet<Cooschmoordinate>();
+            Queue<Cooschmoordinate> InteKollat = new Queue<Cooschmoordinate>();
+            InteKollat.Enqueue(new Cooschmoordinate(0, 0, 0));
+            while (InteKollat.Count() > 0)
+            {
+                Cooschmoordinate pop = InteKollat.Dequeue();
+                List<Cooschmoordinate> Schneigbours = pop.GetNeihbours();
+                foreach (Cooschmoordinate schshhsc in Schneigbours)
+                {
+                    if (schshhsc.IsInPositiveishBounds(MaxX + 1, MaxY + 1, MaxZ + 1) && !Kollat.Contains(schshhsc))
+                        if (Schmoshmoordinates.Contains(schshhsc))
+                        {
+                            ReturnValue++;
+                        }
+                        else
+                        {
+                            InteKollat.Enqueue(schshhsc);
+                            Kollat.Add(schshhsc);
+                        }
+                }
+            }
             return ReturnValue.ToString();
         }
     }
---
+++ Advent2022Tests/Day18Tests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Advent2022;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2022.Tests
+{
+    [TestClass()]
+    public class Day18Tests
+    {
+        [TestMethod()]
+        public void GetPartOneTest()
+        {
+            Day18 day = new Day18("2,2,2\r\n1,2,2\r\n3,2,2\r\n2,1,2\r\n2,3,2\r\n2,2,1\r\n2,2,3\r\n2,2,4\r\n2,2,6\r\n1,2,5\r\n3,2,5\r\n2,1,5\r\n2,3,5");
+            Assert.AreEqual("64", day.GetPartOne());
+        }
+
+        [TestMethod()]
+        public void GetPartTwoTest()
+        {
+            Day18 day = new Day18("2,2,2\r\n1,2,2\r\n3,2,2\r\n2,1,2\r\n2,3,2\r\n2,2,1\r\n2,2,3\r\n2,2,4\r\n2,2,6\r\n1,2,5\r\n3,2,5\r\n2,1,5\r\n2,3,5");
+            Assert.AreEqual("140", day.GetPartTwo());
+        }
+    }
+}
