7ab033963e527091905926342ed19c2d29b4b297
Meowth52 <meowth@meowth.se>
2022-12-19 23:36:35 +0100

    Day 16, some parsing

--- Advent2022/Day16.cs
+++ Advent2022/Day16.cs
@@ -4,16 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.IO;
 
 namespace Advent2022
 {
     public class Day16 : Day
     {
-        List<int> Instructions;
+        string[] Instructions;
+        Dictionary<char, Valve> Valves;
         public Day16(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Instructions = this.ParseListOfInteger(Input);
+            Instructions = this.ParseStringArray(Input);
+            Valves = new Dictionary<char, Valve>();
+            foreach (string s in Instructions)
+            {
+                int flow = this.ParseListOfInteger(s).First();
+                string[] split = s.Split(' ');
+                List<char> routes = new List<char>();
+                for (int i = 9; i < split.Length; i++)
+                {
+                    routes.Add(split[i][0]);
+                }
+                Valves.Add(split[1][0], new Valve(flow, routes));
+            }
         }
         public override Tuple<string, string> GetResult()
         {
@@ -22,7 +36,28 @@ namespace Advent2022
         public string GetPartOne()
         {
             int ReturnValue = 0;
-
+            int Rate = 0;
+            Queue<char> tunnels = new Queue<char>();
+            tunnels.Enqueue('A');
+            for (int i = 1; i <= 30; i++)
+            {
+                Queue<char> NextTunnels = new Queue<char>();
+                foreach (char t in tunnels)
+                {
+                    ReturnValue += Rate;
+                    Valve v = Valves[t];
+                    if (v.Rate > 0)
+                    {
+                        Rate += v.Rate;
+                        v.Rate = 0;
+                        foreach (char r in v.Routes)
+                            NextTunnels.Enqueue(r);
+                    }
+                    foreach (char r in v.Routes)
+                        NextTunnels.Enqueue(r);
+                }
+                tunnels = new Queue<char>(NextTunnels);
+            }
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
@@ -30,6 +65,16 @@ namespace Advent2022
             int ReturnValue = 0;
 
             return ReturnValue.ToString();
+        }
+        public class Valve
+        {
+            public int Rate;
+            public List<char> Routes;
+            public Valve(int rate, List<char> routes)
+            {
+                Rate = rate;
+                Routes = routes;
+            }
         }
     }
 }
---
+++ Advent2022Tests/Day16Tests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Advent2022;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2022.Tests
+{
+    [TestClass()]
+    public class Day16Tests
+    {
+        [TestMethod()]
+        public void GetPartOneTest()
+        {
+            Day16 day = new Day16("Valve AA has flow rate=0; tunnels lead to valves DD, II, BB\r\nValve BB has flow rate=13; tunnels lead to valves CC, AA\r\nValve CC has flow rate=2; tunnels lead to valves DD, BB\r\nValve DD has flow rate=20; tunnels lead to valves CC, AA, EE\r\nValve EE has flow rate=3; tunnels lead to valves FF, DD\r\nValve FF has flow rate=0; tunnels lead to valves EE, GG\r\nValve GG has flow rate=0; tunnels lead to valves FF, HH\r\nValve HH has flow rate=22; tunnel leads to valve GG\r\nValve II has flow rate=0; tunnels lead to valves AA, JJ\r\nValve JJ has flow rate=21; tunnel leads to valve II");
+            Assert.AreEqual("1651", day.GetPartOne());
+        }
+
+        [TestMethod()]
+        public void GetPartTwoTest()
+        {
+            Day16 day = new Day16("Valve AA has flow rate=0; tunnels lead to valves DD, II, BB\r\nValve BB has flow rate=13; tunnels lead to valves CC, AA\r\nValve CC has flow rate=2; tunnels lead to valves DD, BB\r\nValve DD has flow rate=20; tunnels lead to valves CC, AA, EE\r\nValve EE has flow rate=3; tunnels lead to valves FF, DD\r\nValve FF has flow rate=0; tunnels lead to valves EE, GG\r\nValve GG has flow rate=0; tunnels lead to valves FF, HH\r\nValve HH has flow rate=22; tunnel leads to valve GG\r\nValve II has flow rate=0; tunnels lead to valves AA, JJ\r\nValve JJ has flow rate=21; tunnel leads to valve II");
+            Assert.AreEqual("0", day.GetPartTwo());
+        }
+    }
+}
