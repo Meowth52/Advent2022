0270fc805338a632d2b091c52558dfc13390a3bd
Meowth <NoThisIsAPublicRepo@No.No>
2022-12-21 17:24:24 +0100

    fuu

--- Advent2022/Day20.cs
+++ Advent2022/Day20.cs
@@ -24,30 +24,39 @@ namespace Advent2022
             int ReturnValue = 0;
             int count = Instructions.Count - 1;
             List<int> Move = new List<int>(Instructions);
-            for (int i = 0; i <= 3000;)
+            int i = 0;
+            //for (int i = 0; i <= 3000;)
+            //{
+            foreach (int n in Instructions)
             {
-                foreach (int n in Instructions)
-                {
-                    int index = Move.IndexOf(n);
-                    //index = index % count ;
-                    int newIndex = index + n;
-                    newIndex %= count;
-                    if (newIndex < 0)
-                        newIndex += count;
-                    if (newIndex >= count)
-                        newIndex -= count;
-                    Move.Remove(n);
-                    //if (newIndex < index)
-                    //    newIndex++;
-                    //if (newIndex == count + 1)
-                    //    newIndex = 0;
-                    Move.Insert(newIndex, n);
-                    i++;
-                    if (i % 1000 == 0)
-                        ReturnValue += Move[1000 % (count)];
-                }
+                int index = Move.IndexOf(n);
+                //index = index % count ;
+                int newIndex = index + n;
+                newIndex %= count;
+                if (newIndex < 0)
+                    newIndex += count;
+                if (newIndex >= count)
+                    newIndex -= count;
+                Move.Remove(n);
+                //if (newIndex < index)
+                //    newIndex++;
+                //if (newIndex == count + 1)
+                //    newIndex = 0;
+                if (newIndex == 0)
+                    newIndex = count;
+                Move.Insert(newIndex, n);
+                i++;
+                //if (i % 1000 == 0)
+                //{
 
+                //}
             }
+            int schmindex = Move.IndexOf(0);
+
+            ReturnValue += Move[(schmindex + 1000) % Instructions.Count()];
+            ReturnValue += Move[(schmindex + 2000) % Instructions.Count()];
+            ReturnValue += Move[(schmindex + 3000) % Instructions.Count()];
+            //}
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
